acf03b2ef397bdc74cd66d2b9914bda2810eb186
khonello <rkoomson777@gmail.com>
2022-12-12 09:16:17 +0800

    Am done

--- Sockets/Sockets/server.py
+++ Sockets/Sockets/server.py
@@ -8,19 +8,20 @@ import pathlib
 import shutil
 import tempfile
 
+
 addrssQ = queue.Queue(maxsize= 4)
 socksQ = queue.Queue(maxsize= 4)
 
-def str_to_list(arg):
+# def str_to_list(arg):
 
-    lst  = []
-    lst[:0] = arg
+#     lst  = []
+#     lst[:0] = arg
 
-    return lst
+#     return lst
 
 
 # Create function to listen to connections
-def create_sock(IP:str, port:int, conn:int):
+def create_sock(IP:str, port:int, conn:int, buf:int, tmp_folder:str, tmp_file:str, /):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((IP, port))
@@ -37,97 +38,93 @@ def create_sock(IP:str, port:int, conn:int):
 
                 addrssQ.put(addr)
                 socksQ.put(clientSock)
-            else:
-                mssg = 'There is too much traffic, try again later.'.encode('utf-8', 'ignore')
-                clientSock.send(mssg)
-                clientSock.close()
+
+                try:
+                    os.mkdir(tmp_folder)
+                except:
+                    shutil.rmtree(os.path.abspath(tmp_folder))
+                finally:
+                        getsock = socksQ.get(block= True, timeout= None)
+                        mssg = 'ready to receive file...'.encode('utf-8')
+
+                        getsock.send(mssg)
+                        raw_byte = getsock.recv(buf)
+                        print('receiving file...')
+
+                        file = __import__('io').BytesIO(raw_byte)
+
+                        path = pathlib.Path(os.curdir).joinpath(tmp_folder) 
+                        tmp_file_path = path.joinpath(tmp_file).absolute()
+
+                        with open(tmp_file_path, 'wb') as f:
+                            fd = f.fileno()
+
+                            if f.writable() and file.readable():
+                                f.write(file.read())
+                                os.fsync(fd)
+                                    
+                        black = subprocess.Popen(['black', '--target-version', 'py38', tmp_file_path], stderr= subprocess.PIPE, stdout= subprocess.PIPE)                                            # output not needed  
+                        _, __ = black.communicate()
+
+                        pylint = subprocess.Popen(['pylint', '--suggestion-mode=y', '--output-format=colorized', '--reports=y', tmp_file_path], stderr= subprocess.PIPE, stdout= subprocess.PIPE)   # output very important
+                        stdout, stderr = pylint.communicate()  
+
+                        if os.path.isfile(tmp_file_path):
+                
+                            err_path = path.joinpath('pylint_err.txt').absolute(); out_path = path.joinpath('pylint_out.txt').absolute()
+                            err = open(err_path, 'w'); out = open(out_path, 'w')
+
+                            if out.writable() and err.writable():
+                                out.write(stdout.decode()); err.write(stderr.decode())
+
+                                if not err.closed and not out.closed:
+                                    err.close(); out.close() 
+
+                        else:
+                            sys.exit('Something went terribly wrong')
 
 
+                        tmp_nam = os.path.basename(tempfile.mktemp())
+                        archive_path = pathlib.Path(os.curdir).joinpath(f'{tmp_nam}.zip')
 
-def process_req(buf, tmp_folder, tmp_file):
+                        __import__('time').sleep(2.0)
+                        def for_try_block():
+                
+                            shutil.make_archive(tmp_nam, 'zip', path)
+                            with open(archive_path, 'rb') as f:
 
-    try:
-        os.mkdir(tmp_folder)
-    except:
-        ...
-    finally:
-            sock = socksQ.get(block= True, timeout= None)
-            mssg = 'ready to receive file...'.encode('utf-8')
+                                getsock.send(f.read())
+                                print('new file has been sent')
+                            getsock.close()
 
-            sock.send(mssg)
-            raw_byte = sock.recv(buf)
-            print('receiving file...')
+                            os.remove(archive_path)
+                            shutil.rmtree(path, ignore_errors= True)
 
-            file = __import__('io').BytesIO(raw_byte)
+                        #create zipfiles if not existed
+                        try:
+                            for_try_block()
 
-            path = pathlib.Path(os.curdir).joinpath(tmp_folder) 
-            tmp_file_path = path.joinpath(tmp_file).absolute()
+                        except FileExistsError:
 
-            with open(tmp_file_path, 'wb') as f:
-                fd = f.fileno()
+                            os.remove(archive_path)
+                            for_try_block()
 
-                if f.writable() and file.readable():
-                    f.write(file.read())
-                    os.fsync(fd)
-                        
-            black = subprocess.Popen(['black', '--target-version', 'py38', tmp_file_path], stderr= subprocess.PIPE, stdout= subprocess.PIPE)                                            # output not needed  
-            _, __ = black.communicate()
+        else:
+            mssg = 'There is too much traffic, try again later.'.encode('utf-8', 'ignore')
 
-            pylint = subprocess.Popen(['pylint', '--suggestion-mode=y', '--output-format=colorized', '--reports=y', tmp_file_path], stderr= subprocess.PIPE, stdout= subprocess.PIPE)   # output very important
-            stdout, stderr = pylint.communicate()  
+            clientSock.send(mssg)
+            clientSock.close()
 
-            if os.path.isfile(tmp_file_path):
-    
-                err_path = path.joinpath('pylint_err.txt').absolute(); out_path = path.joinpath('pylint_out.txt').absolute()
-                err = open(err_path, 'w'); out = open(out_path, 'w')
+# def process_req(buf, tmp_folder, tmp_file):
 
-                if out.writable() and err.writable():
-                    out.write(stdout.decode()); err.write(stderr.decode())
-
-                    if not err.closed and not out.closed:
-                        err.close(); out.close() 
-
-            else:
-                sys.exit('Something went terribly wrong')
-
-
-            tmp_nam = os.path.basename(tempfile.mktemp())
-            archive_path = pathlib.Path(os.curdir).joinpath(f'{tmp_nam}.zip')
-
-            __import__('time').sleep(2.0)
-            def for_try_block():
-    
-                shutil.make_archive(tmp_nam, 'zip', path)
-                with open(archive_path, 'rb') as f:
-
-                    sock.send(f.read())
-                    print('new file has been sent')
-                sock.close()
-
-                os.remove(archive_path)
-                shutil.rmtree(path, ignore_errors= True)
-
-            #create zipfiles if not existed
-            try:
-                for_try_block()
-
-            except FileExistsError:
-
-                os.remove(archive_path)
-                for_try_block()
-
+#     ...
 
 try:
    PP = sys.argv[1]
 except IndexError:
    PP = 9695
 
-create_sock_args = ['127.0.0.1', PP, 4]
-process_req_args = [4096, 'temp', 're-factored.py']
-
-
+create_sock_args = ['127.0.0.1', PP, 4, 4096, 'temp', 're-factored.py']
 th1 = threading.Thread(target= create_sock, args= create_sock_args)
-th2 = threading.Thread(target= process_req, args= process_req_args)
 
 th1.start()
-th2.start()
